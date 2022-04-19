94299196bbb1530f5d02d4cfb50fd06882aac4d5
unknown <af.jaoulali@gmail.com>
2022-04-19 07:27:14 +0000

    initilisation

---
+++ migrations/Version20220414115657.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220414115657 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE media (id INT AUTO_INCREMENT NOT NULL, vehicule_id INT DEFAULT NULL, src VARCHAR(255) NOT NULL, alt VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, INDEX IDX_6A2CA10C4A4A3511 (vehicule_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C4A4A3511 FOREIGN KEY (vehicule_id) REFERENCES vehicule (id)');
+        $this->addSql('ALTER TABLE commande CHANGE membre_id membre_id INT DEFAULT NULL, CHANGE vehicule_id vehicule_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE media');
+        $this->addSql('ALTER TABLE commande CHANGE membre_id membre_id INT NOT NULL, CHANGE vehicule_id vehicule_id INT NOT NULL');
+    }
+}
